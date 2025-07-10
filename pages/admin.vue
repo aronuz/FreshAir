6a3838af88c0017c4a15f34293cf65014c283993
aronuz <aronuz1@hotmail.com>
2025-07-10 16:36:39 -0400

    Set Typing and Tab view in Admin

--- pages/admin.vue
+++ pages/admin.vue
@@ -5,63 +5,119 @@
       
     <UCard>
       <template #header class="text-xl font-semibold">Admin Panel</template>
-      <UCard>
-        <template #header class="text-xl font-semibold">User Management</template>
-        <div v-if="!users.length && !pending">No Users</div>
-        <template v-else-if="!users.length && pending">
-          <USkeleton v-for="i in 3" class="mx-auto mt-1 h-2 w-full bg-gray-600" as="div"/>
+
+      <UTabs :unmount-on-hide="false" :items="tabItems" class="w-full">
+        <template #content="{ item }">
+          <UCard>
+            <template #header class="text-xl font-semibold">{{ item.label }}</template>
+            <template v-if="item.label === 'User Management'">
+              <div v-if="!users.length && !pending">No Users</div>
+              <template v-else-if="!users.length && pending">
+                <USkeleton v-for="i in 3" class="mx-auto mt-1 h-2 w-full bg-gray-600" as="div"/>
+              </template>
+              <div v-else>
+                <div v-for="user in users" :key="user.user_id" class="flex justify-between p-3 bg-gray-100 rounded">
+                  <UCheckbox @changed="updateSelectedUsers($event, user.user_id)" />
+                  <span>{{ user.title }} - {{ user.phone }}</span><span v-if="user.email">/{{ user.email }}</span><span>&nbsp;- {{ user.role }} - Profile created on: {{ user.created_at }}</span>
+                  <UButton @click="loadUserEvents(user)" label="See Appointments" />
+                
+                  <div class="space-x-2">
+                    <UButton class="bg-blue-500 text-white px-2 py-1 rounded" @click="handleUpdateUser(user)" label="Edit" />
+                    <UButton class="bg-red-500 text-white px-2 py-1 rounded" @click="handleDeleteUsers(user.user_id)" label="Remove" />
+                  </div>
+                </div>
+              </div>
+            </template>
+            <template v-else>
+              <div v-for="page in pages" :key="page.to" class="flex justify-between p-3 bg-gray-100 rounded">
+                <span>{{ page.name }}</span>
+                <UCheckbox v-model="page.allowed" :label="page.allowed ? 'Shown' : 'Hidden'"/>
+                <UFormField v-if="page.allowed" label="Page path">
+                  <USelect v-model="pathPicked[page.name]" :items="[page.to, '/construction']" value-key="id" class="w-full" label="Path" arrow @update:modelValue="updateService(page.name, pathPicked[page.name])"/>
+                </UFormField>
+                <UButton class="bg-blue-500 text-white px-2 py-1 rounded" @click="updatePageAccess(page)" label="Edit" />
+              </div>
+            </template>
+            
+            <template #footer v-if="item.label === 'User Management'">
+              <UButton icon="i-heroicons-plus-circle" color="primary" variant="solid" label="Add" @click="isOpenUser = true"/>
+              <UButton icon="i-heroicons-plus-circle" color="primary" variant="solid" label="Remove" @click="handleDeleteUsers(selectedUsers)"/>
+            </template>
+          </UCard>
         </template>
-        <div v-else>
-          <div v-for="user in users" :key="user.user_id" class="flex justify-between p-3 bg-gray-100 rounded">
-            <UCheckbox @changed="updateSelectedUsers($event, user.user_id)" />
-            <span>{{ user.title }} - {{ user.phone }}</span><span v-if="user.email">/{{ user.email }}</span><span>&nbsp;- {{ user.role }} - Profile created on: {{ user.created_at }}</span>
-            <UButton @click="loadUserEvents(user)" label="See Appointments" />
-          
-            <div class="space-x-2">
-              <UButton class="bg-blue-500 text-white px-2 py-1 rounded" @click="handleUpdateUser(user)" label="Edit" />
-              <UButton class="bg-red-500 text-white px-2 py-1 rounded" @click="handleDeleteUsers(user.user_id)" label="Remove" />
-            </div>
-          </div>
-        </div>
-        <template #footer>
-          <UButton icon="i-heroicons-plus-circle" color="primary" variant="solid" label="Add" @click="isOpenUser = true"/>
-          <UButton icon="i-heroicons-plus-circle" color="primary" variant="solid" label="Remove" @click="handleDeleteUsers(selectedUsers)"/>
-        </template>
-      </UCard>
-      <UserModal v-model="isOpenPages" @saved="handlePageUpdate" />
-      <UCard>
-        <template #header class="text-xl font-semibold">Page Access</template>
-        <div v-for="page in pages" :key="page.to" class="flex justify-between p-3 bg-gray-100 rounded">
-          <span>{{ page.name }}</span>
-          <UCheckbox v-model="page.allowed" :label="page.allowed ? 'Shown' : 'Hidden'"/>
-          <UFormField v-if="page.allowed" label="Page path">
-            <USelect v-model="pathPicked[page.name]" :items="[page.to, '/construction']" value-key="id" class="w-full" label="Path" arrow @update:modelValue="updateService(page)"/>
-          </UFormField>
-          <UButton class="bg-blue-500 text-white px-2 py-1 rounded" @click="updatePageAccess(page)" label="Edit" />
-        </div>
-      </UCard>
+      </UTabs>
+
     </UCard>
   </ClientOnly>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { ref } from 'vue'
-import { array } from 'zod'
+import type { TabsItem } from '@nuxt/ui'
+
+interface userType {
+  title: string,
+  phone: string,
+  email: string | undefined,
+  role: string | undefined,
+  user_id: string,
+  created_at: string,
+}
+
+interface stateType {
+  address?: string | undefined,
+  start_date?: Date | string | undefined,
+  start_time?: string | undefined,
+  end_date?: Date | string | undefined,
+  end_time?: string | undefined,
+  service?: number | undefined,
+  notes?: string | undefined,
+  zip?: string | undefined,
+}
+
+interface pageItem {
+    name: string,
+    to: string,
+    allowed: boolean,
+}
+
+interface pathType {
+  [key: string]: string
+}
+
+const tabItems = ref<TabsItem[]>([
+  {
+    label: 'User Management',
+    icon: 'i-lucide-user',
+  },
+  {
+    label: 'Page Access',
+    icon: 'i-lucide-library',
+  }
+])
 
 const onError = useNuxtApp().$onError
-const users = ref([])
-const selectedUser = ref(null)
-const selectedUsers = ref([])
-const appointments = ref(null)
-const pages = reactive([{ name: 'Home', to: '/dashboard', allowed: true }, 
+const users = ref<userType[]>([])
+const selectedUser = ref<userType | null>(null)
+const selectedUsers = ref<string[]>([])
+const appointments = ref<stateType[]>([])
+
+const isOpenUser = ref(false)
+const isOpenEvents = ref(false)
+
+const pages = reactive([{ name: 'Home', to: '/index', allowed: true }, 
 { name: 'Services', to: '/services', allowed: true }, 
 { name: 'Schedule Service', to: '/booking', allowed: true }, 
 { name: 'Contact Us', to: '/contact', allowed: true }, 
 { name: 'About Us', to: '/about', allowed: true }])
-const isOpenUser = ref(false)
-const isOpenEvents = ref(false)
 
-const error = ref(null)
+const pathPicked: pathType = reactive({
+  'Home': '/index',
+  'Services': '/services', 
+  'Schedule Service': '/booking',
+  'Contact Us': '/contact',
+  'About Us': '/about'
+})
 
 const { getPageAccess,
         updatePageAccess,
@@ -70,9 +126,9 @@ const { getPageAccess,
         fetchAppointments,
         pending } = useFetchQueries()
 
-const updateSelectedUsers = async (event, id) => {
+const updateSelectedUsers = async (event: boolean, id: string) => {
   if (event) selectedUsers.value.push(id)
-  else selectedUsers = selectedUsers.filter(item => item !== id)
+  else selectedUsers.value = selectedUsers.value.filter(item => item !== id)
 }
 
 const handleLoadUsers = async () => {
@@ -89,9 +145,9 @@ const handleLoadUsers = async () => {
   }
 }
 
-const handleDeleteUsers = async (pending, ids) => {
+const handleDeleteUsers = async (ids: string | string[]) => {
   try {
-    const user_ids = !typeof ids === array ? [ids] : ids
+    const user_ids = !Array.isArray(ids) ? [ids] : ids
     const { error, isPending } = await deleteUsers(pending, user_ids)
     pending.value = isPending.value
     if (error) {
@@ -113,25 +169,27 @@ onMounted(async () => {
       onError(500, error)
       return
     }
-    data.forEach((pageItem) => {
-     const item = pages.find((page) => {
-      page.name === pageItem.name
-     })
-     item.to = pageItem.to
-     item.allowed = pageItem.allowed
-    })
+    if(data) {
+      data.forEach((pageItem: pageItem) => {
+      const item = pages.find((page) => {
+        page.name === pageItem.name
+      })
+      item!.to = pageItem.to
+      item!.allowed = pageItem.allowed
+      })
+    }
   }).catch(error => { 
     onError(500, error)
   })
 })  
 
-const handleUpdateUser = async (user) => {
+const handleUpdateUser = async (user: userType) => {
   selectedUser.value = user
   isOpenUser.value = true
 }
 
-const loadUserEvents = async (user) => {
-  const {data, isPending, error, status} = await fetchAppointments(pending, null, user.user_id, true)
+const loadUserEvents = async (user: userType) => {
+  const {data, isPending, error, status} = await fetchAppointments(pending, 0, user.user_id, true)
   pending.value = isPending.value
   if(error){
     onError(status, error)
@@ -146,31 +204,23 @@ watch(isOpenUser, (val) => {
   if (!val) selectedUser.value = null
 })
 
-const updateService = (pageItem, path) => {
-  const item = pages.find(page => page.name = pageItem.name)
-  item.to = path
+const updateService = (pageName: string, path: string) => {
+  const item = pages.find(page => page.name = pageName)
+  item!.to = path
 }
 
-const pathPicked = reactive({
-  'Home': null,
-  'Services': null, 
-  'Schedule Service': null,
-  'Contact Us': null,
-  'About Us': null
-})
-
-const sendEmail = async () => {
-  try {
-    await $fetch('/api/send-email', {
-      method: 'POST',
-      body: emailData.value,
-    });
-  } catch (e) {
-    error.value = e;
-    onError( 500, error.value)
-  }
-  emailData.value = { to: '', subject: '', body: '' };
-};
+// const sendEmail = async () => {
+//   try {
+//     await $fetch('/api/send-email', {
+//       method: 'POST',
+//       body: emailData.value,
+//     });
+//   } catch (e) {
+//     error.value = e;
+//     onError( 500, error.value)
+//   }
+//   emailData.value = { to: '', subject: '', body: '' };
+// };
 
 definePageMeta({
   layout: "default"
