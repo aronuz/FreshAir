c0cbff4c2f7618c043ed901e86eaaa4b146d94ec
aronuz <aronuz1@hotmail.com>
2025-11-04 11:05:24 -0500

    Move and update router.options.ts

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,30 +1,26 @@
 import type { RouterConfig } from '@nuxt/schema'
 
-export const router: RouterConfig = {
+export default <RouterConfig>{
     scrollBehavior(to, _from, savedPosition) {
-        // If there's a saved position (e.g., from using the back button),
-        // return to that position.
-        console.log('Scrolling:',to, savedPosition)
-        debugger
-        if (savedPosition) {
+        // Scroll to a saved position if exists and not at top
+        if (savedPosition && JSON.stringify(savedPosition) !== '{"left":0,"top":0}') {
             return savedPosition
         }
-        debugger
         return new Promise((resolve) => {
+             // Scroll after page loaded
             setTimeout(() => {
                 let scrollObject: { el: string; behavior: ScrollBehavior } | { top: number; behavior: ScrollBehavior }
-                // If navigating to a new route with a hash, scroll to that element.
+                // Scroll to hashed element.
                 if (to.hash) {
                     scrollObject = {
                         el: to.hash,
                         behavior: 'smooth',
                     }
                 } else {
-                    scrollObject = { top: 10, behavior: 'smooth' } // Default scroll to top
+                    scrollObject = { top: 5, behavior: 'smooth' } // Default scroll to top
                 }
-                console.log('Scrolling to:', scrollObject)
                 resolve(scrollObject)
-            }, 500) // Delay to allow page content to load
+            }, 300)
         })
     }
 }
