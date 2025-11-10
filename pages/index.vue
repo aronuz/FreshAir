ecc215ea6bf58a30b81f7e42677603f03e207aa2
aronuz <aronuz1@hotmail.com>
2025-11-10 18:56:38 -0500

    Refactor template

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,65 @@
 <template>
     <UContainer>
-        <section class="pb-12 text-center font-bold bg-gray-100 h-fit w-1/2 mx-auto opacity-80 rounded-lg shadow-md">
-            <h2 class="text-4xl text-gray-800 text-shadow-lg text-shadow-cyan-500 mt-6">Reliable HVAC Services You Can Trust</h2>
-            <p class="text-lg text-gray-600 mt-4 max-w-xl mx-auto">
-            We provide top-notch heating, ventilation, and air conditioning services for residential and commercial clients.
-            Our experienced technicians are dedicated to ensuring your comfort all year round.
-            </p>
-            <div class="mt-8 space-x-4 font-bold">
-            <UButton to="/contact" label="Request Service" color="info" size="lg" />
-            <UButton to="/gallery" label="Our Services" variant="outline" color="info" size="lg" />
-            </div>
+        <section>
+            <UCard class="p-6 text-center font-bold bg-gray-100 h-fit w-fit mx-auto opacity-80 rounded-lg shadow-md">
+                <h2 class="text-4xl text-gray-800 text-shadow-lg text-shadow-cyan-500 mt-6">Reliable HVAC Services You Can Trust</h2>
+                <p class="text-lg text-gray-600 mt-4 max-w-xl mx-auto">
+                We provide top-notch heating, ventilation, and air conditioning services for residential and commercial clients.
+                Our experienced technicians are dedicated to ensuring your comfort all year round.
+                </p>
+                <div class="mt-8 space-x-4 font-bold">
+                <UButton to="/contact" label="Request Service" color="info" size="lg" />
+                <UButton to="/gallery" label="Our Services" variant="outline" color="info" size="lg" />
+                </div>
+            </UCard>
         </section>
 
-        <section class="py-12 bg-gray-100 rounded-lg shadow-md mt-8">
-            <h2 class="text-2xl font-bold text-gray-800 text-center text-shadow-lg text-shadow-cyan-500 mb-6">Why Choose Us?</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div class="text-center">
-                <UIcon name="i-heroicons-cog-6-tooth" class="text-primary-500 text-4xl mx-auto mb-2" />
-                <h3 class="font-semibold text-lg text-gray-700">Expert Technicians</h3>
-                <p class="text-gray-600">Our team consists of highly trained and certified HVAC professionals.</p>
-            </div>
-            <div class="text-center">
-                <UIcon name="i-heroicons-check-circle" class="text-green-500 text-4xl mx-auto mb-2" />
-                <h3 class="font-semibold text-lg text-gray-700">Quality Service</h3>
-                <p class="text-gray-600">We are committed to providing reliable and efficient HVAC solutions.</p>
-            </div>
-            <div class="text-center">
-                <UIcon name="i-heroicons-clock" class="text-yellow-500 text-4xl mx-auto mb-2" />
-                <h3 class="font-semibold text-lg text-gray-700">Prompt Response</h3>
-                <p class="text-gray-600">We understand the urgency of HVAC issues and respond quickly.</p>
-            </div>
-            </div>
+        <section>
+            <UCard class="py-12 bg-gray-100 rounded-lg shadow-md mt-8">
+                <template #header>
+                    <h2 class="text-2xl font-bold text-gray-800 text-center text-shadow-lg text-shadow-cyan-500 mb-6">Why Choose Us?</h2>
+                </template>                
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+                    <div class="text-center">
+                        <UIcon name="i-heroicons-cog-6-tooth" class="text-primary-500 text-4xl mx-auto mb-2" />
+                        <h3 class="font-semibold text-lg text-gray-700">Expert Technicians</h3>
+                        <p class="text-gray-600">Our team consists of highly trained and certified HVAC professionals.</p>
+                    </div>
+                    <div class="text-center">
+                        <UIcon name="i-heroicons-check-circle" class="text-green-500 text-4xl mx-auto mb-2" />
+                        <h3 class="font-semibold text-lg text-gray-700">Quality Service</h3>
+                        <p class="text-gray-600">We are committed to providing reliable and efficient HVAC solutions.</p>
+                    </div>
+                    <div class="text-center">
+                        <UIcon name="i-heroicons-clock" class="text-yellow-500 text-4xl mx-auto mb-2" />
+                        <h3 class="font-semibold text-lg text-gray-700">Prompt Response</h3>
+                        <p class="text-gray-600">We understand the urgency of HVAC issues and respond quickly.</p>
+                    </div>
+                </div>
+            </UCard>
         </section>
 
-        <section class="mt-8 text-center text-white shadow-md">
-            <div v-if="isBookingDown" class="p-4 bg-gray-200 rounded-lg">
-                <ContactForm />
-            </div>
-            <div v-else class="p-4 font-bold bg-secondary-500 rounded-lg">
-                <h2 class="text-3xl text-shadow-lg text-shadow-cyan-500 mb-4">Schedule an Appointment Today!</h2>
-                <p class="text-lg mb-6">Get your HVAC system checked by a professional. Book your appointment today.</p>
-                <UButton to="/booking" label="Book Appointment" color="neutral" size="lg" />
-            </div>
+        <section>
+            <UCard class="mt-8 text-center text-white bg-secondary-500 shadow-md">
+                <template #header>
+                    <div class="text-3xl text-shadow-lg text-shadow-cyan-500 mb-4">
+                        <div v-if="!isBookingDown" class="flex flex-col">
+                            <p>Booking Service is Currently Unavailable.</p>
+                            <p>Please Contact Us Directly.</p>
+                        </div>
+                        <div v-else>
+                            Schedule an Appointment Today!
+                        </div>
+                    </div>
+                </template>
+                <div v-if="isBookingDown" class="p-4 bg-gray-200 rounded-lg">
+                    <ContactForm />
+                </div>
+                <div v-else class="p-4 font-bold rounded-lg">
+                    <p class="text-lg mb-6">Get your HVAC system checked by a professional. Book your appointment today.</p>
+                    <UButton to="/booking" label="Book Appointment" color="neutral" size="lg" />
+                </div>
+            </UCard>
         </section>
 
         <section v-if="showTable" class="py-12 mt-8 bg-white rounded-lg shadow-md">
