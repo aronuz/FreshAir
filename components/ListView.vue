aedbdf620554034cd28fdea2cd5da66a99fa5f5f
aronuz <aronuz1@hotmail.com>
2025-11-14 13:29:36 -0500

    Update ListView.vue
    Update styling and add name

--- components/ListView.vue
+++ components/ListView.vue
@@ -7,9 +7,12 @@
                         <template v-for="(group, key, index) in pageEvent" :key="key">
                             <div class="flex justify-end">{{ key }}</div>
                             <UCard v-for="event in group" :key="event.id" class="flex flex-col" :class="`even:bg-sky-${index + 1 < 7 ? index + 1 : 7}00 odd:bg-sky-${group.length + (2 * index + 1) < 9 ? group.length + (2 * index + 1) : 9}00`">
-                            <span v-if="event.service">{{ event.service }}</span>
-                            <span v-else>Service</span> 
-                            @ {{ event.address }}
+                                <strong>
+                                    <span v-if="event.service">{{ event.service }}</span>
+                                    <span v-else>Service</span>
+                                </strong> 
+                                <span class="ml-2">@ <strong>{{ event.address }}</strong></span>
+                                <span v-if="event.title" class="ml-2">for <strong>{{ event.title }}</strong></span> 
                             </UCard>
                         </template>
                     </template>
