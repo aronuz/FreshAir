cbd0acc9483d2da9d6b83f24d8327f55f8ea1789
aronuz <aronuz1@hotmail.com>
2025-11-26 02:23:56 -0500

    Update screenSize.ts
    set to return reactive value

--- plugins/screenSize.ts
+++ plugins/screenSize.ts
@@ -2,9 +2,9 @@ import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 
 export default defineNuxtPlugin((nuxtApp) => {
     const breakpoints = useBreakpoints(breakpointsTailwind) 
-        const bp = breakpoints?.greaterOrEqual('lg') ? 'lg' : 
+        const bp = computed(() => breakpoints?.greaterOrEqual('lg') ? 'lg' : 
             breakpoints?.greaterOrEqual('md') ? 'md' : 
             breakpoints?.greaterOrEqual('sm') ? 'sm' : 'xs'
-
-    nuxtApp.provide('screenSize', bp);
+        )
+    nuxtApp.provide('screenSize', bp.value);
 })
