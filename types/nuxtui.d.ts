7fd93c542cd7e5491fd8be84b474fe8c2a5b231e
aronuz <aronuz1@hotmail.com>
2026-01-16 08:08:24 -0500

    Create nuxtui.d.ts

---
+++ types/nuxtui.d.ts
@@ -0,0 +1,11 @@
+declare module 'nuxt/ui' {
+  import type { DefineComponent } from 'vue'
+  
+  export const UModal: DefineComponent<any, any, any>
+  export const UCard: DefineComponent<any, any, any>
+  export const UButton: DefineComponent<any, any, any>
+  export const UForm: DefineComponent<any, any, any>
+  
+  export default {}
+}
+export {}
